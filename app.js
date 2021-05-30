0784f5f85f06138340a0e84e2baec070fa3cba27
Jay Thomas <thelastjet17@gmail.com>
2021-05-29 20:54:42 -0700

    completing exercises 1,2 and 3

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = "Jay";
+let lastName = "Thomas";
+var age = 53;
 // Exercise 2
-
+let fullName = `${firstName} ${lastName}`;
+console.log(fullName);
 // Exercise 3
+let city = "Tucson";
+let pastime = "learning to code";
+let myStory = `Hello! My name is ${fullName}. I live in ${city} which is in Arizona. One of my favorite pastimes is ${pastime}.`
+console.log(myStory);
